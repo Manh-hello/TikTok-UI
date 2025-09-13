2ae7aa1ad5494654b90ca90cac35930b7338ece1
Manh-hello <manhnd.23it@vku.udn.vn>
2025-09-13 16:08:01 +0700

    Cài babel plugin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Content from './Content'
 
 function App() {
+  const [moun, setMoun] = useState(1);// true là giá trị mặc định, moun mặc định là true, từ lần setMoun thì sẽ đổi giá trị , set Moun có thể là hàm, component đc render lại sau khi setMoun
+  const handle = () =>{
+    // setMoun(moun+1);
+    // setMoun(moun+1);
+    // setMoun(moun+1); // như này thì moun lấy giá trị của lần trước chứ không phải lấy + 3 lần
+    setMoun(prev => prev + 1);
+    setMoun(prev => prev + 1);
+    setMoun(prev => prev + 1); // như này thì moun mỗi lần gọi setMoun đều đc gán lại giá trị, khi truyền hàm vào thì giá trị trả về sẽ là inittial (moun)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ padding: 20 }}>
+      <button onClick={handle}>click</button>
     </div>
-  );
-}
 
+);
+// {/* {moun && <Content />} */}
+// {/* <h1>{moun}</h1> */}
+}
 export default App;
---
+++ src/Content.js
@@ -0,0 +1,10 @@
+import { useEffect } from 'react'
+function Content(){
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    );
+}
+
+export default Content
